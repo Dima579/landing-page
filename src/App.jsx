7110d91019b3680de5c3ed9550cfade1be67ca00
Alex Dimakopoulos <47307066+Dima579@users.noreply.github.com>
2025-11-11 00:27:31 +0000

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Hero from './components/hero.jsx'
+import Hero from './components/Hero.jsx'
 import SiteFeatures from './components/SiteFeatures.jsx'
 
 function App() {
